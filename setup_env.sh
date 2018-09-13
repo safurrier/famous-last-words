edf01906b302e03d0832279257f18dc6a394caf7
safurrier <safurrier@gmail.com>
2018-09-13 09:28:47 -0700

    Altered setup_env.sh script to ask for project name rather than edit config file

--- setup_env.sh
+++ setup_env.sh
@@ -3,28 +3,34 @@ git init
 git add .
 git commit -a -m 'first commit'
 
-# Read in config file variables using this function
-function parse_yaml {
+## Read in config file variables using this function
+#function parse_yaml {
+#
+#   local prefix=$2
+#   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+#   sed -ne "s|^\($s\):|\1|" \
+#        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+#        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+#   awk -F$fs '{
+#      indent = length($1)/2;
+#      vname[indent] = $2;
+#      for (i in vname) {if (i > indent) {delete vname[i]}}
+#      if (length($3) > 0) {
+#         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+#         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+#      }
+#   }'
+#}
 
-   local prefix=$2
-   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
-   sed -ne "s|^\($s\):|\1|" \
-        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
-   awk -F$fs '{
-      indent = length($1)/2;
-      vname[indent] = $2;
-      for (i in vname) {if (i > indent) {delete vname[i]}}
-      if (length($3) > 0) {
-         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
-      }
-   }'
-}
-eval $(parse_yaml config.yaml)
+# For use if config variables needed
+#eval $(parse_yaml config.yaml)
 
+printf "\n\n\n"
+read -p "Enter project name (will be used as name for a new conda env): " name
+project_name=${name:-data_science_project}
+printf "\n\n\n"
 # Create conda environment with project name
-echo y | conda create --name $project_name python jupyter ipykernel
+conda create -y --name $project_name python jupyter ipykernel
 # Activate environment
 source activate $project_name
 # Attach kernel of this environment for use with jupyter 
