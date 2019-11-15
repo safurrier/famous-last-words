27862d5bdb0b0c261235df22da32a5f4f8844020
safurrier <safurrier@gmail.com>
2019-11-15 09:11:44 -0700

    Updated requirements.txt to include pathlib. Added Dockerfile to .devcontainer. Added setup_env_silent.sh which sets up python conda env without any prompts. Must set project name in file otherwise will use a default env name

---
+++ setup_env_silent.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+
+# initialize git repo and make first commit
+#git init
+#git add .
+#git commit -a -m 'first commit'
+
+project_name=default_project_env
+printf "Project name is: "$project_name
+printf "\nThis will be conda env name"
+# Create conda environment with project name
+conda create -y --name $project_name python=3.6
+# Activate environment
+source activate $project_name
+# Attach kernel of this environment for use with jupyter 
+#python -m ipykernel install --user --name $project_name --display-name $project_name
+
+# Install Requirements  
+# conda install -y --file requirements.txt
+pip install -r requirements.txt
+
+python -m spacy download en_core_web_sm
+
+# Install src as local package
+pip install -e .
+
+# Install utils from github
+cd src
+git clone https://github.com/safurrier/data-science-utils
+mv data-science-utils utils
+cd ..
